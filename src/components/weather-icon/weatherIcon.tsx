099b3caab84c7c3e17df78aa78d64bb9fefa5f8f
aaron <aaronkim662@gmail.com>
2021-07-01 15:52:53 -0400

    edited icon

--- src/components/weather-icon/weatherIcon.tsx
+++ src/components/weather-icon/weatherIcon.tsx
@@ -1,8 +1,10 @@
 import './weatherIcon.sass';
 
 const WeatherIcon = () => {
-  return <div>
-
+  return <div id = 'weather-icon'>
+    <div className = 'weather-icon-element'>A</div>
+    <div className = 'weather-icon-element'>Weather</div>
+    <div className = 'weather-icon-element'>App</div>
   </div>
 }
 
