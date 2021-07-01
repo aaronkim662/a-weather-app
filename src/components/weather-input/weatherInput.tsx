702ab9d51adeffe885fcd54d3422ca186f191480
aaron <aaronkim662@gmail.com>
2021-07-01 14:55:52 -0400

    edited input

--- src/components/weather-input/weatherInput.tsx
+++ src/components/weather-input/weatherInput.tsx
@@ -1,4 +1,6 @@
 import React, { InputHTMLAttributes } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTimes, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import './weatherInput.sass';
 
 interface InputType extends InputHTMLAttributes<HTMLInputElement>{
@@ -14,7 +16,23 @@ const WeatherInput: React.FC<InputType> = (props) => {
   }
   return(
     <form id = 'form-element' onSubmit = {(e) => props.getWeather(e)}>
-      <input value = {props.location} placeholder = 'Enter a city' onChange = {(e) => props.setLocation(e.target.value)}/>
+      <FontAwesomeIcon
+        id = 'search-weather-icon'
+        icon = {faSearch}
+      />
+      <input
+        value = {props.location}
+        placeholder = 'Enter a city'
+        onChange = {(e) => props.setLocation(e.target.value)}
+      />
+      {props.location.length > 0 ?
+        <FontAwesomeIcon
+          id = 'clear-content-icon'
+          className = 'clear-element'
+          icon = {faTimesCircle}
+          onClick={clearContent}
+        /> : <div className = 'clear-element'></div>
+      }
     </form>
   )
 }
