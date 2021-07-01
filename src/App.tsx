d15d7c72c03af82fe4db7990e48d2170ea408b86
aaron <aaronkim662@gmail.com>
2021-07-01 14:38:28 -0400

    edited app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import './App.css';
 import { useActions } from './hooks/useActions';
 import { useTypedSelector } from './hooks/useTypedSelector';
-import WeatherInput from './components/weather-input/weather-input';
+import WeatherInput from './components/weather-input/weatherInput';
+import { useState } from 'react';
 
 const WeatherApp: React.FC = () => {
 
-  // const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('')
 
   const { SearchWeather } = useActions();
 
   const { data, error, loading } = useTypedSelector((state) => state.weather);
 
-  const getWeather = async () => {
-    SearchWeather('seattle')
+  const getWeather = async (e: any) => {
+    e.preventDefault()
+    SearchWeather(location)
     console.log('here', data)
   };
 
   return (
       <div>
-        Hello
-        <button onClick = {getWeather}>Weather</button>
-        <WeatherInput />
+        <WeatherInput location=  {location} getWeather = {getWeather} setLocation ={setLocation}/>
       </div>
-
   )
 }
 
