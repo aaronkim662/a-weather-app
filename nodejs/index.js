85b7cc5752fe2e6a70c4b06b4a9278d842d3abe4
aaron <aaronkim662@gmail.com>
2021-07-02 17:05:59 -0400

    edited back end

--- nodejs/index.js
+++ nodejs/index.js
@@ -17,7 +17,7 @@ app.get('/weather/:location', (req, res) => {
       res.json(response.data);
     })
     .catch(error => {
-      console.log(error);
+      console.log(error.response)
     });
 });
 
