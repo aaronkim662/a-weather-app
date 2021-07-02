901de647bf709970382c13c95d8764068db28c35
aaron <aaronkim662@gmail.com>
2021-07-02 17:05:09 -0400

    edited container

--- src/components/weather-container/weatherContainer.tsx
+++ src/components/weather-container/weatherContainer.tsx
@@ -1,7 +1,24 @@
 import './weatherContainer.sass';
+import { setTemperature } from '../../math/setTemperature';
+import LoadingContainer from '../loading-container/loadingContainer';
+import ErrorContainer from '../error-container/errorContainer';
 
-const WeatherContainer: React.FC = () => {
-  return <div></div>
+interface ContainerType {
+  data: any;
+  error: string | null;
+  loading: boolean;
+  degree: string;
+}
+
+const WeatherContainer: React.FC<ContainerType> = (props) => {
+  // const data = store.getState();
+  const temp = props.data[0].main;
+
+  return <div id = 'weather-container'>
+    <div id = 'main-temp'>{setTemperature(temp?.temp, props.degree)}&deg;</div>
+    <div id = 'feels-like' className = 'temp-element'>Feels Like: {setTemperature(temp?.feels_like, props.degree)}&deg;</div>
+    <div className = 'temp-element'>Feels Like: {setTemperature(temp?.feels_like, props.degree)}&deg;</div>
+  </div>
 }
 
 export default WeatherContainer;
