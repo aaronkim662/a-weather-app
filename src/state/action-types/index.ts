787fe31438384dd5846362c4351d3d6a42681586
aaron <aaronkim662@gmail.com>
2021-07-05 16:49:22 -0400

    added more values for weather

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -9,14 +9,22 @@ type TempState = {
   temp: number;
   temp_max: number;
   temp_min: number;
+  humidity: number;
+  pressure: number;
 }
 
 type WeatherState = {
+  name: string | null;
   inner_description: string | null;
   main_description: string | null;
+}
+
+type WindState = {
+  wind: number;
 }
 
 export type SearchState = {
   description: WeatherState
   main: TempState;
+  wind: WindState;
 }
