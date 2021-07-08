9a4b20199366892e50b39c604bf4c3408d91d409
aaron <aaronkim662@gmail.com>
2021-07-08 19:38:17 -0400

    edited loading

--- src/components/loading-container/loadingContainer.tsx
+++ src/components/loading-container/loadingContainer.tsx
@@ -1,8 +1,13 @@
 import './loadingContainer.sass';
+import spinner from './spinner.gif';
 
 const LoadingContainer = () => {
   return <div>
-    Loading
+    <img
+      src= {spinner}
+      className = 'loading-spinner'
+      alt="Loading..."
+      />
   </div>
 };
 
